7e2628bae1eab00d20b6de11f5fa91d8e6f332bc
HassanNawazish <hassannawazishrasool1@gmail.com>
2026-02-08 00:34:57 +0100

    Testing API

---
+++ EmployeeAdminPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace EmployeeAdminPortal.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected ApplicationDbContext()
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
+ 
