102bb2e3eb8c6aaef2db4678b43a3fa5c34ed02b
HassanNawazish <hassannawazishrasool1@gmail.com>
2026-03-31 18:58:12 +0200

    minor update

--- test/api/Dtos/Stock/StockDto.cs
+++ test/api/Dtos/Stock/StockDto.cs
@@ -6,7 +6,6 @@ namespace api.Dtos.Stock
         public string Symbol { get; set; } = string.Empty;
         public string CompanyName { get; set; } = string.Empty;
         public decimal Purchase { get; set; }
-        public decimal LastDiv { get; set; }
         public string Industry { get; set; } = string.Empty;
         public long MarketCap { get; set; }
     }
