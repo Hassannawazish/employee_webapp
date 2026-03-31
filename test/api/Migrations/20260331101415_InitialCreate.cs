60d685edb676ff6d604038687fea3994910a831e
HassanNawazish <hassannawazishrasool1@gmail.com>
2026-03-31 12:15:39 +0200

    minor fix

---
+++ test/api/Migrations/20260331101415_InitialCreate.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Stock_StockId",
+                table: "Comments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Stock",
+                table: "Stock");
+
+            migrationBuilder.RenameTable(
+                name: "Stock",
+                newName: "Stocks");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Stocks",
+                table: "Stocks",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Stocks_StockId",
+                table: "Comments",
+                column: "StockId",
+                principalTable: "Stocks",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Stocks_StockId",
+                table: "Comments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Stocks",
+                table: "Stocks");
+
+            migrationBuilder.RenameTable(
+                name: "Stocks",
+                newName: "Stock");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Stock",
+                table: "Stock",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Stock_StockId",
+                table: "Comments",
+                column: "StockId",
+                principalTable: "Stock",
+                principalColumn: "Id");
+        }
+    }
+}
