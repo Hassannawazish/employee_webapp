bde216d2c9533495e458153cc1414ceaa7f7358f
HassanNawazish <hassannawazishrasool1@gmail.com>
2026-04-13 18:05:35 +0200

    UI with card display

---
+++ test/frontend/src/Components/Card/Card.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+type Props = {}
+
+const Card = (props: Props) => {
+  return <div className='card'>
+        <img
+            src="https://cdn.mos.cms.futurecdn.net/g3k8sf4sNtXPpTeevjoKEW-650-80.jpg.webp"
+            alt="Image"
+        />
+        <div className="details">
+            <h2>AAPL</h2>
+            <p>$110</p>
+        </div>
+        <p className='infon'>official key products</p>
+    </div>;
+};
+
+export default Card
